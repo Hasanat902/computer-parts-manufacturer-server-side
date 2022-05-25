6fb8afb6c148b0e4ffd4b64db30958aba3b337bd
Hasanat902 <abulhasanat902@gmail.com>
2022-05-25 13:03:09 +0600

    created single user order api

--- index.js
+++ index.js
@@ -46,11 +46,20 @@ async function run(){
             res.send(reviews);
         });
 
+        app.get('/order', async(req, res) => {
+            const email = req.query.email;
+            const query = {email: email};
+            const cursor = orderCollection.find(query);
+            const orders = await cursor.toArray();
+            res.send(orders);
+        })
+
         app.post('/order', async(req, res) => {
             const order = req.body;
             const result = await orderCollection.insertOne(order);
             res.send(result);
-        })
+        });
+
         
     }
     finally{
